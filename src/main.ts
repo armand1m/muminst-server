bcd3cf0bc14a485fe00ebeac46bd418e639db005
Rafael Martins <rmartins@godaddy.com>
2020-10-04 17:48:47 -0300

    feat: format and add cors

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,31 @@
-
 import dotenv from 'dotenv'
 import express from 'express'
-import {mumbleClient} from './mumbleService'
+import cors from 'cors'
+import { mumbleClient } from './mumbleService'
+
 dotenv.config()
 
-const app = express()
 const port = process.env.PORT
+const corsOptions = {
+    origin: 'http://localhost.com:3000',
+    optionsSuccessStatus: 200,
+}
 
+const app = express()
+app.use(cors())
 
 app.get('/', (_, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/current-channel', (_, res) => {
-  res.json(mumbleClient.getCurrentChannel())
+    res.json(mumbleClient.getCurrentChannel())
 })
 
 app.get('/channels', (_, res) => {
-  res.json(mumbleClient.getChannels())
+    res.json(mumbleClient.getChannels())
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
