d87f66f0989e8de74150b0d47fd9a0a0e1c1a784
Armando Magalhães <amagalhaes@netflix.com>
2020-10-06 18:50:38 +0200

    fix: hardcoded rafael bullshit

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -2,17 +2,12 @@ import fs from 'fs'
 
 const audioPath = process.env.AUDIO_PATH || ''
 
-console.log(audioPath)
-
 const filterFormat = (files: string[]) =>
     files.filter((file) => file.match(/\.(?:wav|mp3)$/i))
 
 const listSounds = async () =>
     filterFormat(
-        await fs.promises.readdir(
-            '/home/roboto/Codes/mumblebot/muminst-server/src/audio',
-            {}
-        )
+        await fs.promises.readdir(audioPath, {})
     )
 
 export const fileService = {
