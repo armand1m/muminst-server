c31d79f2521d8b6dd62f1995bf57dac79529669a
Armando Magalhães <amagalhaes@netflix.com>
2020-10-17 13:47:44 +0200

    fix: validates the mime type of the buffer

--- src/handlers/upload.ts
+++ src/handlers/upload.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
+import { UploadedFile } from 'express-fileupload';
+import { fromBuffer } from 'file-type';
 import { isEmpty } from 'ramda';
 import createHttpError from 'http-errors';
 import HttpStatusCodes from 'http-status-codes';
 import { db } from '../db';
 import { makeSound } from '../model/Sound';
 import { buildFilePath } from '../util/buildFilePath';
+
+const isValidFile = async (file: UploadedFile) => {
+  const mime = await fromBuffer(file.data);
+  return mime?.ext === "mp3" || mime?.mime === "audio/mpeg";
+};
 
 /**
  * Upload Handler
@@ -29,17 +36,19 @@ export const uploadHandler = async (
     const allFilesMove = await Promise.all(
       Object.values(req.files).map(async (file) => {
         try {
+          const isValid = await isValidFile(file);
+
+          if (!isValid) {
+            throw new Error(`This type of file is not accepted.`);
+          }
+
           const sound = await makeSound(file);
           const hashCheck = db.sounds.getByFileHash(sound.fileHash);
 
           if (hashCheck) {
-            return {
-              status: 'error',
-              data: {
-                filename: file.name,
-                reason: `Sound already exists with sound name "${hashCheck.name}"`,
-              },
-            };
+            throw new Error(
+              `Sound already exists with sound name "${hashCheck.name}"`
+            );
           }
 
           await db.sounds.add(sound);
