43faf8fdeec34282e11f070dae1e355e524ee3f5
Armando Magalhães <amagalhaes@netflix.com>
2021-05-17 23:47:04 +0200

    fix: make retrial work

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,11 +1,11 @@
 import Discord from 'discord.js';
 import { Logger } from 'pino';
-// @ts-ignore
-import retry from 'async-await-retry';
 import { DiscordProperties } from '../config';
 import { LockStore } from '../stores/LockStore';
 import { buildFilePath } from '../util/buildFilePath';
 import { ChatClient } from './chatClient';
+
+const retry = require('async-await-retry');
 
 let _currentClient: ChatClient | undefined;
 let _currentConnection: Discord.VoiceConnection | undefined;
@@ -31,16 +31,32 @@ const setupClient = (
     });
   };
 
-  return new Promise<ChatClient>((resolve) => {
+  return new Promise<ChatClient>((resolve, reject) => {
     logger.info('Configuring Discord client..');
 
     const client = new Discord.Client();
 
     client.on('ready', () => {
       logger.info('Discord client is ready.');
+
       if (client.user) {
         logger.info(`Logged in as ${client.user.tag}`);
       }
+
+      resolve({
+        isLocked: () => lockStore.getState().isLocked,
+        playFile,
+        playSound: (sound) => {
+          const filepath = buildFilePath(sound);
+          /** TODO: Send message to discord channel as well */
+          playFile(filepath);
+        },
+      });
+    });
+
+    client.on('error', (err) => {
+      logger.info('Discord Client suffered an error');
+      reject(err);
     });
 
     client.on('message', async (message) => {
@@ -67,16 +83,6 @@ const setupClient = (
     });
 
     client.login(token);
-
-    resolve({
-      isLocked: () => lockStore.getState().isLocked,
-      playFile,
-      playSound: (sound) => {
-        const filepath = buildFilePath(sound);
-        /** TODO: Send message to discord channel as well */
-        playFile(filepath);
-      },
-    });
   });
 };
 
@@ -86,9 +92,9 @@ export const getDiscordClient = async (
   props: DiscordProperties
 ) => {
   if (!_currentClient) {
-    _currentClient = await retry(() =>
-      setupClient(logger, lockStore, props)
-    );
+    _currentClient = await retry(() => {
+      return setupClient(logger, lockStore, props);
+    });
   }
 
   return _currentClient;
